2de62a21adb0594d2fefe8bf9636d54df5c99896
seiichi <arai.seiichi@classmethod.jp>
2020-02-18 19:22:01 +0900

    20200218の進捗コミット

--- complete.js
+++ complete.js
@@ -43,7 +43,22 @@ document.addEventListener('DOMContentLoaded', () => {
     function generateSave(url) { //creates the save button
       const currentDate = new Date(Date.now()).toDateString();
       saveButton.onclick = () => {
-        chrome.downloads.download({url: url, filename: `${currentDate}.${format}`, saveAs: true});
+        console.log(url)
+        fetch("https://gijirokustack-bucket83908e77-14rpvbs4ce6xf.s3-ap-northeast-1.amazonaws.com/sample_01.mp3", {
+          method: "PUT", // *GET, POST, PUT, DELETE, etc.
+          mode: "cors", // no-cors, cors, *same-origin
+          cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+          credentials: "same-origin", // include, same-origin, *omit
+          headers: {
+              "Content-Type": "audio/mpeg",
+          },
+          redirect: "follow", // manual, *follow, error
+          referrer: "no-referrer", // no-referrer, *client
+          body: url, // 本文のデータ型は "Content-Type" ヘッダーと一致する必要があります
+      }).then(response => {
+        console.log(response)
+      });
+        // chrome.downloads.download({url: url, filename: `${currentDate}.${format}`, saveAs: true});
       };
       saveButton.style.display = "inline-block";
     }
